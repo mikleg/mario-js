9515350d54aa6908fddcef64475901fa37295e46
Mikle Gorbunov <mihailgorbunov74@gmail.com>
2017-08-22 15:51:27 -0700

    render pyramid to the DOM instead of  the console

--- mario.js
+++ mario.js
@@ -18,15 +18,17 @@ function printPyramid(height) {
 
     for (var j = 0; j < height; j++) {
         var rez = '';
-        for (var i = 0; i < (height - (1+j)); i++) {
-            rez = rez + ' ';
-        }
+        for (var i = 0; i < (height - (1+j)); i++)
+            rez = rez + String.fromCharCode(160);
         for (i = 0; i < j+2 ; i++)
             rez = rez + '#';
+        var node = document.createElement("P");
+        var textnode = document.createTextNode(rez);
+        node.appendChild(textnode);
+        document.getElementById("pyramid").appendChild(node);
 
-        console.log(rez);
     }
-        // TODO
-    // print that pyramid!
-
+       // TODO
 }
+
+//printPyramid(5);
