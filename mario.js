638c47923f8e6b486ebdcdcfe050e0038b0e7ac8
Mikle Gorbunov <mihailgorbunov74@gmail.com>
2017-09-18 19:52:17 -0700

    added bonus mission

--- mario.js
+++ mario.js
@@ -17,18 +17,18 @@ function printPyramid(height) {
 
 
     for (var j = 0; j < height; j++) {
-        var rez = '';
-        for (var i = 0; i < (height - (1+j)); i++)
-            rez = rez + String.fromCharCode(160);
-        for (i = 0; i < j+2 ; i++)
-            rez = rez + '#';
-        var node = document.createElement("P");
-        var textnode = document.createTextNode(rez);
-        node.appendChild(textnode);
-        document.getElementById("pyramid").appendChild(node);
-
+        var row = document.createElement("DIV");
+        for (var i = 0; i < (height - (1+j)); i++) {
+            var block = document.createElement("DIV");
+            block.setAttribute("class", "whiteBlock");
+            row.appendChild(block);
+        }
+        for (i = 0; i < j+2 ; i++) {
+            block = document.createElement("DIV");
+            block.setAttribute("class", "redBlock");
+            row.appendChild(block);
+        }
+        document.getElementById("pyramid").appendChild(row);
     }
-       // TODO
-}
 
-//printPyramid(5);
+}
